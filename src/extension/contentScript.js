8f882d09ed5e9a829ed84ccd882dfff9c2cf6a80
Sean <smith.seanm@yahoo.com>
2020-06-26 22:17:18 -0700

    boilerplate communication between app.jsx, background.js, and contentscript.js

--- src/extension/contentScript.js
+++ src/extension/contentScript.js
@@ -1,2 +1,19 @@
-console.log('this is the content script')
-document.getElementById('root').innerText = 'THIS IS RECOILIZE'
+console.log('this is the content script refreshed')
+
+const sendMessage = message => {
+  console.log('in the send message in the content script');
+  console.log("*")
+  console.log('        ', message, ' <-- message');
+  console.log("*")
+
+  chrome.runtime.sendMessage(message)
+}
+
+// Listening for message from injected script - inject.js
+window.addEventListener('message', e => {
+  //console.log('what do i look like in the window event listener?')
+  // console.log(e.data, ' <--- e.data')
+  // console.log('----')
+  // Making sure the event listened too is from the window 
+  sendMessage(e.data);
+});
