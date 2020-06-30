e28be8cb1f8465597f6581500bbd70de3c118be5
skang1004 <saejin.kang1004@gmail.com>
2020-06-30 12:32:27 -0700

    got zoom functionality and drag functionality working, need to fix the actual nodes and paths for the atom state that gets passed in from background.js, need to refactor AtomTree.jsx

--- src/app/components/AtomTree.jsx
+++ src/app/components/AtomTree.jsx
@@ -1,20 +1,24 @@
 import React, { useRef, useState, useEffect } from 'react';
 import * as d3 from 'd3';
+import Zoom from './Zoom.jsx';
 
 function AtomTree(props) {
-  console.log('initial atom state in atomTree', props.snapshotHistory);
-  // legend
-  const svg = d3.select('#canvas');
-
   // set the heights and width of the tree to be passed into treeMap
   const width = 500;
   const height = 1500;
 
+  // legend for svg canvas
+  const svgContainer = d3
+    .select('#canvas')
+    .attr('width', width)
+    .attr('height', height);
+
+  const g = svgContainer
+    .append('g')
+    .attr('transform', `translate(${width / 2 + 4}, ${height / 2 + 2})`);
+
   // initial state taken from backgroundScript
   const [snapshotHistory, setSnapshotHistory] = useState(props.snapshotHistory);
-  const [loading, setLoading] = useState(true);
-  // const [atomState, setAtomState] = useState({});
-
   useEffect(() => {
     setSnapshotHistory(props.snapshotHistory);
   });
@@ -25,10 +29,7 @@ function AtomTree(props) {
 
   // ***** need to parse data *****
   const makeTree = (obj) => {
-    // every name should be val
-    // every children should be array
     if (!obj) return;
-    console.log('obj inside makeTree', obj);
 
     let result = [];
     let keys = Object.keys(obj);
@@ -64,7 +65,6 @@ function AtomTree(props) {
     });
     return result;
   };
-  // need to make this atom state be dynamic to the current snapshot
 
   const atomState = {
     name: 'Recoil Root',
@@ -89,12 +89,6 @@ function AtomTree(props) {
   let paths = finalMap.links();
   console.log('paths', paths);
 
-  // returns a flat array of objects
-  // renders nodes onto the component
-  let nodes = hierarchyNodes.descendants();
-  console.log('nodes', nodes);
-
-  const g = svg.append('g');
   g.selectAll('.link')
     .data(paths)
     .enter()
@@ -104,13 +98,80 @@ function AtomTree(props) {
       'd',
       d3
         .linkRadial()
-        .angle((d) => {
-          return d.x;
-        })
+        .angle((d) => d.x)
         .radius((d) => d.y)
     );
 
-  const node = g.selectAll('.node').data(nodes);
+  // returns a flat array of objects
+  // renders nodes onto the component
+  let nodes = hierarchyNodes.descendants();
+  console.log('nodes', nodes);
+
+  const node = g
+    .selectAll('.node')
+    .data(nodes)
+    .enter()
+    .append('g')
+    .attr('class', 'node')
+    .attr('transform', (d) => {
+      console.log('d inside node', d);
+      // return `translate(${d.event.transform})`;
+    });
+
+  node
+    .append('circle')
+    .attr('r', 15)
+    .on('mouseover', function (d) {
+      d3.select(this).transition(100).duration(20).attr('r', 25);
+    })
+    .on('mouseout', function (d) {
+      d3.select(this).transition().duration(300).attr('r', 15);
+    });
+
+  node.call(
+    d3.drag().on('start', dragStarted).on('drag', dragged).on('end', dragEnded)
+  );
+
+  svgContainer.call(
+    d3
+      .zoom()
+      .extent([
+        [0, 0],
+        [width, height],
+      ])
+      .scaleExtent([0, 8])
+      .on('zoom', zoomed)
+  );
+
+  function dragStarted() {
+    d3.select(this).raise();
+    g.attr('cursor', 'grabbing');
+  }
+
+  function dragged(d) {
+    d3.select(this)
+      .attr('dx', (d.x = d3.event.x))
+      .attr('dy', (d.y = d3.event.y));
+  }
+
+  function dragEnded() {
+    g.attr('cursor', 'grab');
+  }
+
+  function zoomed() {
+    g.attr('transform', d3.event.transform);
+  }
+  // const node = g.selectAll('.node').data(nodes);
+
+  //trying to work on zoom container
+
+  // const zoom = d3.zoom().on('zoom', zoomed);
+  // const zoomContainer = document.getElementById('zoomContainer');
+  // zoomContainer.call(zoom);
+
+  // let transform = d3.event.transform;
+
+  // zoomContainer.attr('transform', transform.toString());
 
   return (
     <div className='AtomTree' width='100vw' height='100vh'>
@@ -120,6 +181,10 @@ function AtomTree(props) {
         height='100000'
         width='100000'
       ></svg>
+      <rect id='zoomContainer' style={{ opacity: 0 }} pointerEvents='all'>
+        {/* {paths}
+        {nodes} */}
+      </rect>
     </div>
   );
 }
