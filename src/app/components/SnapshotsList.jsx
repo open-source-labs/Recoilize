1ebc1c5e273fa216da6fefbbcfd81e5892c804c7
Jonathan Escamilla <jonathanescamilla1@gmail.com>
2020-07-01 16:22:40 -0700

    send index to background.js of index to timetravel

--- src/app/components/SnapshotsList.jsx
+++ src/app/components/SnapshotsList.jsx
@@ -1,11 +1,11 @@
 // renders a list of all of the snapshots that were taking
 import React from 'react';
 
-const SnapshotsList = ({ curRender, setCurRender, snapshots }) => {
+const SnapshotsList = ({ curRender, setCurRender, snapshotHistory, setSnapshotTimeTravelIndex }) => {
   // this is where we grab snapshots using one of recoil's hooks and paste in an array
-  const listOfSnapshots = snapshots.reduce((acc, curSnap, i) => {
+  const listOfSnapshots = snapshotHistory.reduce((acc, curSnap, i) => {
     acc.push(
-      <button
+      <div
         className='individualSnapshot'
         key={i}
         // setState functionality to update curRender
@@ -13,8 +13,11 @@ const SnapshotsList = ({ curRender, setCurRender, snapshots }) => {
           setCurRender(i);
         }}
       >
-        <li>{i}</li>
-      </button>
+        <li>{i} <button onClick= {() => {
+          console.log(`Time Travel to ${i}`);
+          setSnapshotTimeTravelIndex(i);
+          }}>Time Travel</button></li>
+      </div>
     );
     return acc;
   }, []);
