f36d2ca41cab97f8e1838888c3ffd5a62a5f7668
Jonathan Escamilla <jonathanescamilla1@gmail.com>
2020-07-01 22:15:20 -0700

    improve styling w/ updated pseudo code

--- src/app/components/Diff.jsx
+++ src/app/components/Diff.jsx
@@ -1,14 +1,30 @@
 // renders the difference between the most recent state change and the previous
-import React from 'react';
-import { diff, formatters } from 'jsondiffpatch';
-import ReactHtmlParser from 'react-html-parser';
+import React, { useState } from "react";
+import { diff, formatters } from "jsondiffpatch";
+import ReactHtmlParser from "react-html-parser";
 
 const Diff = ({ oldSnap, newSnap }) => {
+  //
+  const [rawToggle, setRawToggle] = useState(false);
   // diffing between oldSnap && newSnap
   const delta = diff(oldSnap, newSnap);
   // string of html with comparisons
   const html = formatters.html.format(delta, oldSnap);
-  return <div className='Diff'>{ReactHtmlParser(html)}</div>;
+  // conditionally render changes or not
+  rawToggle ? formatters.html.showUnchanged() : formatters.html.hideUnchanged();
+
+  return (
+    <div className="Diff">
+      <button
+        onClick={() => {
+          setRawToggle(!rawToggle);
+        }}
+      >
+        Toggle Raw
+      </button>
+      {ReactHtmlParser(html)}
+    </div>
+  );
 };
 
 export default Diff;
