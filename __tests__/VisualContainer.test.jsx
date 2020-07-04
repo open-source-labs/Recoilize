daac54db29662ad629741b15d3c6e5a663eea0c4
skang1004 <saejin.kang1004@gmail.com>
2020-07-03 20:52:09 -0700

    testing the VisualContainer test file

--- __tests__/VisualContainer.test.jsx
+++ __tests__/VisualContainer.test.jsx
@@ -2,22 +2,14 @@ import React from 'react';
 import {shallow, configure} from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import VisualContainer from '../src/app/Containers/VisualContainer.jsx';
-import Diff from '../src/app/components/Diff.jsx';
-// import Tree from '../src/app/components/TreeDir/Tree.jsx';
-import AtomTree from '../src/app/components/AtomTree.jsx';
 
 configure({adapter: new Adapter()});
 
 describe('Visual Container Tests', () => {
   let wrapper;
-  let navProps;
+
   beforeAll(() => {
     wrapper = shallow(<VisualContainer />);
-    // navProps = {
-    //   Diff: <Diff />,
-    //   Tree: <Tree />,
-    //   Visualizer: <AtomTree />,
-    // };
   });
   it('Visual Container should render NavBar component', () => {
     const navLength = wrapper.find('.VisualContainer');
