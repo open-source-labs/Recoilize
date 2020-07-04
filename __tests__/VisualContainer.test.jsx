c7a612cac54eb2d02302f6c9bcee2797bc8a1a78
skang1004 <saejin.kang1004@gmail.com>
2020-07-03 20:46:51 -0700

    making sure test file still works with new typescript refactoring

--- __tests__/VisualContainer.test.jsx
+++ __tests__/VisualContainer.test.jsx
@@ -3,7 +3,7 @@ import {shallow, configure} from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import VisualContainer from '../src/app/Containers/VisualContainer.jsx';
 import Diff from '../src/app/components/Diff.jsx';
-import Tree from '../src/app/components/TreeDir/Tree.jsx';
+// import Tree from '../src/app/components/TreeDir/Tree.jsx';
 import AtomTree from '../src/app/components/AtomTree.jsx';
 
 configure({adapter: new Adapter()});
@@ -13,11 +13,11 @@ describe('Visual Container Tests', () => {
   let navProps;
   beforeAll(() => {
     wrapper = shallow(<VisualContainer />);
-    navProps = {
-      Diff: <Diff />,
-      Tree: <Tree />,
-      Visualizer: <AtomTree />,
-    };
+    // navProps = {
+    //   Diff: <Diff />,
+    //   Tree: <Tree />,
+    //   Visualizer: <AtomTree />,
+    // };
   });
   it('Visual Container should render NavBar component', () => {
     const navLength = wrapper.find('.VisualContainer');
