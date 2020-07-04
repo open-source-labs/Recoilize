f79c7a682729b4df8b84442360910a249ab77069
skang1004 <saejin.kang1004@gmail.com>
2020-07-03 19:41:47 -0700

    passed first enzyme test for AtomTree component

--- __tests__/Visualizer.test.jsx
+++ __tests__/Visualizer.test.jsx
@@ -1,15 +1,53 @@
 import React from 'react';
-const { configure, shallow, mount } = require('enzyme');
-const Adapter = require('enzyme-adapter-react-16');
+import {configure, shallow, mount} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 import AtomTree from '../src/app/components/AtomTree.jsx';
-const { interpolate } = require('d3');
+import * as d3 from 'd3';
+import makeTree from '../src/app/utils/makeTreeConversion';
 
 // newer enzyme versions require an adapter for React v16
-configure({ adapter: new Adapter() });
+configure({adapter: new Adapter()});
 
-describe('Atom tree visualizer test', () => {
-  const Visualizer = shallow(<AtomTree />);
-  it('Data taken from background.js should be parsed into d3-readble object', () => {
-    expect(true).toEqual(false);
+describe('Atom tree visualizer unit test', () => {
+  const propSnapshot = {
+    dummyAtom1: {
+      contents: {hello: [], hi: []},
+      nodeDeps: [],
+      nodetoNodeSubscriptions: [],
+      type: 'RecoilState',
+    },
+    listState: {
+      contents: [{text: 'list item'}, {text: 'list item'}, {text: 'list item'}],
+      nodeDeps: [],
+      nodetoNodeSubscriptions: ['selectorTest', 'stateLengths'],
+      type: 'RecoilState',
+    },
+    listState2: {
+      contents: [{text: 'list item'}, {text: 'list item'}, {text: 'list item'}],
+      nodeDeps: [],
+      nodetoNodeSubscriptions: ['stateLengths'],
+      type: 'RecoilState',
+    },
+    selectorTest: {
+      contents: 'test',
+      nodeDeps: ['listState'],
+      nodetoNodeSubscriptions: [],
+      type: 'RecoilValueReadOnly',
+    },
+    stateLengths: {
+      contents: 6,
+      nodeDeps: ['listState', 'listState2'],
+      nodetoNodeSubscriptions: [],
+      type: 'RecoilValueReadOnly',
+    },
+  };
+  let Visualizer;
+  beforeAll(() => {
+    Visualizer = shallow(<AtomTree />);
+  });
+  //   console.log('d3', d3);
+  //   console.log('test component', Visualizer);
+  it(`<AtomTree /> should render an svg with id 'canvas' `, () => {
+    expect(Visualizer.find('svg').is('#canvas')).toEqual(true);
   });
 });
