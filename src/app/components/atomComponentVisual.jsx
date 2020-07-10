8cfcac4424ee50ef9e4456f27c325f5e1398afae
Sean <smith.seanm@yahoo.com>
2020-07-10 12:27:36 -0700

    initialize new atomComponentVisual file

---
+++ src/app/components/atomComponentVisual.jsx
@@ -0,0 +1,8 @@
+import React, {useState, useEffect} from 'react';
+import * as d3 from 'd3';
+
+function atomComponentVisual({newSnap}) {
+  const {filteredSnapshot, componentAtomTree} = newSnap;
+}
+
+export default atomComponentVisual;
