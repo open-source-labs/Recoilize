1638773924a0f9e16e8a92d46779cdce66112b3f
skang1004 <saejin.kang1004@gmail.com>
2020-07-13 14:52:07 -0700

    installed react testing library

---
+++ __tests__/Visualizer_Enzyme.test.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import {configure, shallow, mount} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import Visualizer from '../src/app/components/Visualizer';
+import * as d3 from 'd3';
+import makeTree from '../src/app/utils/makeTreeConversion';
+
+// newer enzyme versions require an adapter for React v16
+configure({adapter: new Adapter()});
+
+describe('Atom tree visualizer unit test', () => {
+  const propSnapshot = {
+    dummyAtom1: {
+      contents: {hello: [], hi: []},
+      nodeDeps: [],
+      nodetoNodeSubscriptions: [],
+      type: 'RecoilState',
+    },
+    listState: {
+      contents: [{text: 'list item'}, {text: 'list item'}, {text: 'list item'}],
+      nodeDeps: [],
+      nodetoNodeSubscriptions: ['selectorTest', 'stateLengths'],
+      type: 'RecoilState',
+    },
+    listState2: {
+      contents: [{text: 'list item'}, {text: 'list item'}, {text: 'list item'}],
+      nodeDeps: [],
+      nodetoNodeSubscriptions: ['stateLengths'],
+      type: 'RecoilState',
+    },
+    selectorTest: {
+      contents: 'test',
+      nodeDeps: ['listState'],
+      nodetoNodeSubscriptions: [],
+      type: 'RecoilValueReadOnly',
+    },
+    stateLengths: {
+      contents: 6,
+      nodeDeps: ['listState', 'listState2'],
+      nodetoNodeSubscriptions: [],
+      type: 'RecoilValueReadOnly',
+    },
+  };
+  let Visualizer;
+  beforeAll(() => {
+    Visualizer = shallow(<Visualizer newSnap={propSnapshot} />);
+  });
+  it(`<AtomTree /> should render an svg with id 'canvas' `, () => {
+    expect(Visualizer.find('svg').is('#canvas')).toEqual(true);
+  });
+  //   it('Expect AtomTree to render d3 elements', () => {
+  //     const allD3 = Visualizer.find('svg').find('g');
+  //     expect(allD3.length).toEqual(35);
+  //   });
+});
