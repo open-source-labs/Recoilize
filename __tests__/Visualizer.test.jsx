e65358f28f955f2a94a819fe72203fe57328caa9
skang1004 <saejin.kang1004@gmail.com>
2020-07-14 12:10:38 -0700

    creating snapshot tests for atom/selector tree visualizer

--- __tests__/Visualizer.test.jsx
+++ __tests__/Visualizer.test.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import Visualizer from '../src/app/components/Visualizer';
 import {
   render,
   fireEvent,
+  cleanup,
   waitFor,
   screen,
   findByTestId,
@@ -10,8 +12,57 @@ import {
 import '@testing-library/jest-dom/extend-expect';
 import '@babel/polyfill';
 
-test('loads and displays visualizer', async () => {
-  const {getAllByTestId} = render(<Visualizer />);
-  const visualizerLength = await getAllByTestId('canvas');
-  console.log('length of visualizer', visualizerLength[0]);
+const propSnapshot = {
+  dummyAtom1: {
+    contents: {hello: [], hi: []},
+    nodeDeps: [],
+    nodetoNodeSubscriptions: [],
+    type: 'RecoilState',
+  },
+  listState: {
+    contents: [{text: 'list item'}, {text: 'list item'}, {text: 'list item'}],
+    nodeDeps: [],
+    nodetoNodeSubscriptions: ['selectorTest', 'stateLengths'],
+    type: 'RecoilState',
+  },
+  listState2: {
+    contents: [{text: 'list item'}, {text: 'list item'}, {text: 'list item'}],
+    nodeDeps: [],
+    nodetoNodeSubscriptions: ['stateLengths'],
+    type: 'RecoilState',
+  },
+  selectorTest: {
+    contents: 'test',
+    nodeDeps: ['listState'],
+    nodetoNodeSubscriptions: [],
+    type: 'RecoilValueReadOnly',
+  },
+  stateLengths: {
+    contents: 6,
+    nodeDeps: ['listState', 'listState2'],
+    nodetoNodeSubscriptions: [],
+    type: 'RecoilValueReadOnly',
+  },
+};
+
+afterEach(cleanup);
+
+it('it renders without crashing', () => {
+  const {getByTestId} = render(<Visualizer />);
+  expect(getByTestId('canvas')).toBeTruthy();
+});
+
+it('should match snapshot', () => {
+  const {asFragment} = render(<Visualizer />);
+  expect(asFragment()).toMatchSnapshot();
+});
+
+it('should render Recoil Root as text', () => {
+  const {getByTestId} = render(<Visualizer />);
+  expect(getByTestId('canvas')).toHaveTextContent('Recoil Root');
+});
+
+it('if data gets passed into Visualizer', () => {
+  const {asFragment} = render(<Visualizer filteredCurSnap={propSnapshot} />);
+  expect(asFragment()).toMatchSnapshot();
 });
