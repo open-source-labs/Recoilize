30765444482227218894896bdd80bfa7e9be1dea
Anthony Lin <anthonylin@MacBook-Pro.local>
2020-08-17 11:09:28 -0700

    update

--- src/app/components/App/index.tsx
+++ src/app/components/App/index.tsx
@@ -3,6 +3,7 @@ import MainContainer from '../../Containers/MainContainer';
 import {stateSnapshot} from '../../../types';
 // importing the diff to find difference
 import {diff, formatters} from 'jsondiffpatch';
+import {array} from 'prop-types';
 
 const LOGO_URL = `https://public.bl.files.1drv.com/y4mFC_icIYGiJ2zg4zuUUlrZjGfCu6KwMsjC0Ik0TTaWOxfQ6fdrSbp-KAlqXN-xEvJz_eoYwbqk99pbLNYJsgF0zWR78SXorrfzZ6FwlwxE_E2LattKf92A_63IpKV7LrxK6ajTihA4jMoUb_0g1UbV6M2kfNCIBPCgLS_790mZgWpXp1A2KhGLkkq_UxFX3XBlxVDVTD3lPtefbfhZAD6QW9wIQWlnwwcJVitU9itn24?access_token=EwDwAq1DBAAUzl/nWKUlBg14ZGcybuC4/OHFdfEAAY08AkuWBBUZDe/Sb4ypiAuxFNBnJRT4mw/zktTaSv86GXEcq8XWN4Y467p4%2bwwW%2bB/d2ZYsRsVrNYLVCctgS0sI9LAu5IEHLlFVl/8ChD/1S4JeVeEmrug5qJdAOHcWeBBZID9Ym97lrYio7pBpS06xVBPHLCgVE3nRnRmavzo83pnz61G4jRWagEgPyR2l5Kkrqi2Q0vB6km1j2SrL2BFLU0XuAhWaLIuG6xjrCO46R2k8VxcLC35cSJorlhRK%2bFTpWkv418a3gC6E/3r089dbWCWE3YpapZJnAByBGijgDiy/haXOtWd0/Fz2b/nAnV7L8U8upUVmGU%2btF5Kfu0oDZgAACKMcCrzcFHcqwAEf2BzhuSyDaIxZwTD95O6RokANWN0qCBjKPvVlMB4tuFoJe9m/DKYlq%2boHaJjl9ZTvG8%2blLe2Y2NvSlA0YEA%2bUg5iuKkRlUHEp2smSmdS0lOjOtO1EqzQVkIPfn5a7yZu%2bB0Za4h2SIHpp/awM3KCcDG6NYXBBN33MGphoRHIp7vuTqDnVMcJ/H8O7egZ3KO0rywE8cfYAyDossD0dOO6QchN5qlUrkLmyv5Ffrkr7e%2bXDnRR46brDiNjVsHox1IEMg%2bhs4A2pmZTJjN8JAKz2By4MDKlXTRbipauiCaH5sECSOujJyzt3lTfi46Xb%2bv6166nqa1KkiaviWERkS1eaDkvmtGr0CcoDIupJLGsiH0a/uUj4/KE6Ms1NJk1rw8iX0FC700MtOIhcF9xTM2f7Ifg9cnqOypkGp4atRYxE6ut99lcqXwDR8rp5j7tst1C/%2b1v81OzM7J%2bA2pJ3pADghILwAGaBJ5/Ift2011oTKsl1A1Towl2k6TlooDA9kmIhxECu64eYTpBa4l9WkuqMuDeSvqUAK9/9e1LsFoegtZ/ABYlEPUzW2obvODQj6E7zuSvHT67rWeODku83e5XpBQI%3d`;
 
@@ -13,6 +14,7 @@ const App: React.FC = () => {
 
   // todo: created selected to update array
   const [selected, setSelected] = useState([]);
+  console.log('this is the selected ', selected);
 
   // todo: Create algo that will clean up the big setsnapshothistory object, now and before
   let [filter, setFilter] = useState([]);
@@ -31,7 +33,6 @@ const App: React.FC = () => {
     backgroundConnection.onMessage.addListener(msg => {
       if (msg.action === 'recordSnapshot') {
         // ! creating algo to make sure selected array is correct -- make sure selected is always correct
-        console.log('this is the selected ', selected);
         if (!msg.payload[1] || filter.length === 0) {
           // ensures we only set initially
           const arr = [];
@@ -41,8 +42,6 @@ const App: React.FC = () => {
           setSelected(arr);
         } else {
           // todo: We need to add if there are new keys inside of the obj
-
-          // compare msg.payload.length - 2 and msg.payload.length - 1
           console.log(
             'this is the msg.payload.length-2 ',
             msg.payload[msg.payload.length - 2],
@@ -51,8 +50,12 @@ const App: React.FC = () => {
             'this is the msg.payload.length-1 ',
             msg.payload[msg.payload.length - 1],
           );
+          // for (let key in msg.payload[msg.payload.length - 1]){
+          //   if (!msg.payload[msg.payload.length - 1]){
+          //     arr.push({name: key})
+          //   }
+          // }
         }
-        // we just want to add the keys that are new between the two
 
         // ! Set the snapshot history state
         setSnapshotHistory(msg.payload);
