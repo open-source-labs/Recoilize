e8b44455d481c13ade63f35dae5ae5388407a70f
Steven_Nguyen <stevennnn13@gmail.com>
2020-08-19 22:13:00 -0700

    change color of node when click on legend and fix legend feature

--- src/app/components/AtomComponent/index.tsx
+++ src/app/components/AtomComponent/index.tsx
@@ -201,7 +201,7 @@ const AtomComponentVisual: React.FC<AtomComponentVisualProps> = ({
           return d.children || d._children ? 'middle' : 'middle';
         })
         .text((d: any) => d.data.name)
-        .style('font-size', `4.5rem`)
+        .style('font-size', `7.5rem`)
         .style('fill', 'white');
 
       let nodeUpdate = nodeEnter.merge(node);
@@ -354,7 +354,7 @@ const AtomComponentVisual: React.FC<AtomComponentVisualProps> = ({
         if (d.data.recoilNodes && d.data.recoilNodes.length) {
           if (d.data.recoilNodes.includes(selectedRecoilValue[0])) {
             // Color of atom or selector when clicked on in legend
-            return 'white';
+            return 'yellow';
           }
 
           let hasAtom = false;
@@ -379,7 +379,7 @@ const AtomComponentVisual: React.FC<AtomComponentVisualProps> = ({
         return 'gray';
       }
     }
-  }, [componentAtomTree, rawToggle]);
+  }, [componentAtomTree, rawToggle, selectedRecoilValue]);
 
   return (
     <div className="AtomComponentVisual">
