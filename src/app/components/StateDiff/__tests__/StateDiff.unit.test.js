276d17a8a9727ff6b08852fea91a5eea0fab90ce
Justin Choo <kchoosun@gmail.com>
2020-08-28 16:21:43 -0700

    State Diff componenet unit testing complete. all passed

--- src/app/components/StateDiff/__tests__/StateDiff.unit.test.js
+++ src/app/components/StateDiff/__tests__/StateDiff.unit.test.js
@@ -2,7 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Diff from '../Diff';
 
-import {render, cleanup, getByText, fireEvent} from '@testing-library/react';
+import {
+  render,
+  cleanup,
+  getByText,
+  fireEvent,
+  debug,
+} from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import {
   filteredPrevSnapMock,
@@ -15,21 +21,26 @@ const mockProps = {
   filteredPrevSnap: filteredPrevSnapMock,
   filteredCurSnap: filteredCurSnapMock,
 };
+describe('Diff Component', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Diff />, div);
+  });
+});
 
 describe('Raw Toggle Button', () => {
   beforeAll(cleanup);
   afterAll(cleanup);
-  const {getByText, debug} = render(<Diff />);
-  const rawToggle = getByText('Raw');
 
   it('raw button should be color #989898 at initial render', () => {
-    expect(rawToggle).toHaveStyle('color: #989898');
+    const {getByText} = render(<Diff />);
+    expect(getByText('Raw')).toHaveStyle('color: #989898');
   });
 
   it('raw button should change to color #E6E6E6 after clicked', () => {
-    fireEvent.click(rawToggle);
-    debug();
-    expect(rawToggle).toHaveStyle('color: #E6E6E6');
+    const {getByText} = render(<Diff />);
+    fireEvent.click(getByText('Raw'));
+    expect(getByText('Raw')).toHaveStyle('color: #E6E6E6');
   });
 });
 
