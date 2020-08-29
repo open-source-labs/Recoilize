80dbd11206801749b410e6eea82170e2d176f81d
Henry Taing <hktaing@gmail.com>
2020-08-29 14:12:02 -0700

    Pseudocode problematic tests

--- src/app/components/ComponentGraph/__tests__/AtomComponentContainer.unit.test.js
+++ src/app/components/ComponentGraph/__tests__/AtomComponentContainer.unit.test.js
@@ -5,25 +5,28 @@ import * as d3 from 'd3';
 
 afterEach(cleanup);
 
-xit('testing to see if the component is properly rendered', () => {
-  // This test gets 'TypeError: Cannot read property 'baseVal' of undefined
-  // because Jest doesn't fully support testing SVGs yet
-  const atomTree = {children: []};
-  const {component, debug} = render(
-    <AtomComponentVisualContainer componentAtomTree={atomTree} />,
-  );
-  debug();
-});
-
 describe('The canvas ', () => {
+  // Returns a selection but the selection is empty
   const getCanvas = () => {
     return d3.select('#canvas');
   };
   it('should be created ', () => {
+    console.log('canvas: ', getCanvas());
     expect(getCanvas()).not.toBeNull();
   });
-
+  // Since selection is empty, we can't get the height information
   xit('should have the correct height', function () {
+    console.log('canvas width: ', d3.select('#canvas').attr('width'));
     expect(getCanvas().attr('width')).toBe(500);
+  });
+
+  xit('testing to see if the component is properly rendered', () => {
+    // This test gets 'TypeError: Cannot read property 'baseVal' of undefined
+    // because Jest doesn't fully support testing SVGs yet
+    const atomTree = {children: []};
+    const {component, debug} = render(
+      <AtomComponentVisualContainer componentAtomTree={atomTree} />,
+    );
+    debug();
   });
 });
