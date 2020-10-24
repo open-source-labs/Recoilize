37c781eb68a17daf77274521b9191bfd78189a28
aaronyang024@gmail.com <aaronyang024@gmail.com>
2020-10-23 17:23:56 -0700

    Updated title in devtool panel

--- src/extension/build/devtools.js
+++ src/extension/build/devtools.js
@@ -1,6 +1,6 @@
 // The initial script that loads the extension into the DevTools panel.
 chrome.devtools.panels.create(
-  'Production', // title of devtool panel
+  'Recoilize', // title of devtool panel
   '../assets/covalent-recoil-logo.jpg', // icon of devtool panel
   'panel.html', // html of devtool panel
 );
