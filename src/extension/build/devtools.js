777c1d7eed3fef886a1ececa9f303bd1b391ab4b
jaimebaik <jaimebaik5@gmail.com>
2020-11-17 10:30:08 -0800

    changed name

--- src/extension/build/devtools.js
+++ src/extension/build/devtools.js
@@ -1,6 +1,6 @@
 // The initial script that loads the extension into the DevTools panel.
 chrome.devtools.panels.create(
-  'Recoilize', // title of devtool panel
+  'Recoilize-Production', // title of devtool panel
   '../assets/covalent-recoil-logo.jpg', // icon of devtool panel
   'panel.html', // html of devtool panel
 );
