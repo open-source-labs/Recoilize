fc796b9576f7d52d02bcc6f3fb0bc4d8a846f017
Janis-H <janis_h.aguilar@yahoo.com>
2020-11-29 05:02:29 -0800

    further reduced prop drilling with Context API from VisualContainer downwards

--- src/app/Containers/VisualContainer.tsx
+++ src/app/Containers/VisualContainer.tsx
@@ -1,4 +1,4 @@
-import React, {useContext, useState} from 'react';
+import React, {createContext, useContext, useState} from 'react';
 import Diff from '../components/StateDiff/Diff';
 import NavBar from '../components/NavBar/NavBar';
 import Metrics from '../components/Metrics/MetricsContainer';
@@ -22,11 +22,28 @@ interface ZoomState {
   k: number;
 }
 
+interface CheckedContext {
+  checked: boolean;
+  setChecked: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+interface ThrottleDisplayContext {
+  throttleDisplay: string;
+  setThrottleDisplay: React.Dispatch<React.SetStateAction<string>>;
+}
+
+interface ZoomStateContext {
+  zoomState: ZoomState;
+  setZoomState: React.Dispatch<React.SetStateAction<ZoomState>>;
+}
+
 type navTypes = {
   [tabName: string]: JSX.Element;
 };
 
-const cleanComponentAtomTree = (inputObj: componentAtomTree): componentAtomTree => {
+const cleanComponentAtomTree = (
+  inputObj: componentAtomTree,
+): componentAtomTree => {
   const obj = {} as componentAtomTree;
   let counter = 0;
   const innerClean = (inputObj: any, outputObj: any, counter: number = 0) => {
@@ -68,7 +85,7 @@ const cleanComponentAtomTree = (inputObj: componentAtomTree): componentAtomTree 
   innerClean(inputObj, obj, counter);
 
   //ensure that the root element's actual duration is included in outObj
-  if(inputObj.actualDuration){
+  if (inputObj.actualDuration) {
     obj.actualDuration = inputObj.actualDuration;
   }
 
@@ -120,31 +137,28 @@ const VisualContainer: React.FC<VisualContainerProps> = ({
     'State Tree': <Tree filteredCurSnap={filteredCurSnap} />,
     // tree visualizer of components showing atom/selector relationships
     'Component Graph': (
-      <AtomComponentVisualContainer
-        componentAtomTree={componentAtomTree}
-        cleanedComponentAtomTree={cleanedComponentAtomTree}
-        filteredCurSnap={filteredCurSnap}
-        x={x}
-        y={y}
-        k={k}
-        setZoomState={setZoomState}
-      />
+      <zoomStateContext.Provider value={{zoomState: {x, y, k}, setZoomState}}>
+        <AtomComponentVisualContainer
+          componentAtomTree={componentAtomTree}
+          cleanedComponentAtomTree={cleanedComponentAtomTree}
+          filteredCurSnap={filteredCurSnap}
+        />
+      </zoomStateContext.Provider>
     ),
 
     // atom and selector subscription relationship
     'Atom Network': <Network filteredCurSnap={filteredCurSnap} />,
 
     // individual snapshot visualizer
-    'Metrics': <Metrics cleanedComponentAtomTree={cleanedComponentAtomTree} />,
+    Metrics: <Metrics cleanedComponentAtomTree={cleanedComponentAtomTree} />,
 
     // settings tab that doesn't want to be in quotes because too cool for school
     Settings: (
-      <Settings
-        checked={checked}
-        setChecked={setChecked}
-        throttleDisplay={throttleDisplay}
-        setThrottleDisplay={setThrottleDisplay}
-      />
+      <checkedContext.Provider value={{checked, setChecked}}>
+        <throttleDisplayContext.Provider value={{throttleDisplay, setThrottleDisplay}}>
+          <Settings />
+        </throttleDisplayContext.Provider>
+      </checkedContext.Provider>
     ),
   };
   // array of all nav obj keys
@@ -160,4 +174,7 @@ const VisualContainer: React.FC<VisualContainerProps> = ({
   );
 };
 
+export const checkedContext = createContext<CheckedContext>(null);
+export const throttleDisplayContext = createContext<ThrottleDisplayContext>(null);
+export const zoomStateContext = createContext<ZoomStateContext>(null);
 export default VisualContainer;
--- src/app/components/Settings/SettingsContainer.tsx
+++ src/app/components/Settings/SettingsContainer.tsx
@@ -5,28 +5,13 @@ import StateSettings from './StateSettings';
 import ThrottleSettings from './ThrottleSettings';
 import AtomSettings from './AtomSettings';
 
-interface SettingsProps {
-  checked: boolean;
-  setChecked: React.Dispatch<React.SetStateAction<boolean>>;
-  throttleDisplay: string;
-  setThrottleDisplay: React.Dispatch<React.SetStateAction<string>>;
-}
-
 // renders the difference between the most recent state change and the previous
-const Settings: React.FC<SettingsProps> = ({
-  checked,
-  setChecked,
-  throttleDisplay,
-  setThrottleDisplay,
-}) => {
+const Settings: React.FC = () => {
   return (
     <div className="Settings">
       <AtomSettings />
-      <StateSettings checked={checked} setChecked={setChecked} />
-      <ThrottleSettings
-        throttleDisplay={throttleDisplay}
-        setThrottleDisplay={setThrottleDisplay}
-      />
+      <StateSettings />
+      <ThrottleSettings />
     </div>
   );
 };
