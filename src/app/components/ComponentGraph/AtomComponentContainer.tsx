5031683904bfe54fe317563607b8a9c2f69e927a
Janis-H <janis_h.aguilar@yahoo.com>
2020-12-07 10:47:30 -0800

    Removed Unused Component

--- src/app/components/ComponentGraph/AtomComponentContainer.tsx
+++ src/app/components/ComponentGraph/AtomComponentContainer.tsx
@@ -49,15 +49,6 @@ const AtomComponentVisualContainer: React.FC<AtomComponentVisualContainerProps> 
         selectors={selectors}
         setStr={setStr}
       />
-      {/* <div id="legendFlexContainer">
-        <AtomSelectorLegend
-          selectedRecoilValue={selectedRecoilValue}
-          setSelectedRecoilValue={setSelectedRecoilValue}
-          atoms={atoms}
-          selectors={selectors}
-          str={str}
-        />
-      </div> */}
     </div>
   );
 };
