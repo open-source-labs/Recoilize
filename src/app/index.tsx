a4c3d2eea44664c7f5234f7e0f974b554972b236
razananisathar <frazana.oa@gmail.com>
2021-03-26 08:40:39 +0530

    wip - update throttle settings state

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -4,6 +4,7 @@ import App from './components/App';
 import {store} from './state-management/index';
 import {Provider} from 'react-redux';
 
+console.log('main app store', store);
 ReactDOM.render(
   <Provider store={store}>
     <App />
--- src/app/state-management/index.tsx
+++ src/app/state-management/index.tsx
@@ -1,9 +1,11 @@
 import {configureStore} from '@reduxjs/toolkit';
+import throttleReducer from './slices/ThrottleSlice';
 
 export const store = configureStore({
   reducer: {
-      
+    throttle: throttleReducer,
   },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
