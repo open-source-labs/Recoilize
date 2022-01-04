2f8c39c99ffd5c8dc67e9293c9db80e9ac5f53dd
Harvey Nwynn <harveynwynn@gmail.com>
2022-01-03 20:00:25 -0800

    create TravelContainer component

--- src/app/Containers/MainContainer.tsx
+++ src/app/Containers/MainContainer.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import SnapshotsContainer from './SnapshotContainer';
 import VisualContainer from './VisualContainer';
+import TravelContainer from './TravelContainer';
 
 const MainContainer: React.FC = () => {
   return (
     <div className="MainContainer">
+      <TravelContainer />
       <SnapshotsContainer />
       <VisualContainer />
     </div>
---
+++ src/app/Containers/TravelContainer.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TravelContainer: React.FC = () => {
+  return (
+    <div className="travel-container">
+      <button>Click here</button>
+    </div>
+  );
+};
+
+export default TravelContainer;
