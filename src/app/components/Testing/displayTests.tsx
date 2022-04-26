e28f0ae99a0c69fbdbce38dc4eec4a4f8d6aed0c
Ryan <ryanwallace1396@gmail.com>
2022-04-26 08:10:43 -0700

    Created a component to display the tests dependent on the chose selector from our dropdown
    Co-authored-by: Will Chu <williamchu9@gmail.com>
    Co-authored-by: Adam Allison <allisonadam82@gmail.com>
    Co-authored-by: Jordan Rice <theoriginaljordanrice@gmail.com>

--- src/app/components/Testing/displayTests.tsx
+++ src/app/components/Testing/displayTests.tsx
@@ -1,12 +1,60 @@
 import React, {useState, useEffect} from 'react';
 import {useRecoilState, useRecoilValue, useSetRecoilState} from 'recoil';
 
-const DisplayTests: React.FC = () => {
-  return (
-    <div>
-      <p>Expect current atom through selector to equal [expected] [params]</p>
-    </div>
-  );
+const DisplayTests: React.FC<any> = (props) => {
+  const {currentSelector, currentAtom, currentAtomValue, toBeValue, setToBeValue, parameters, setParameters} = props;
+
+  // use displayedSelector to check if a new selector is chose
+  // if truthy, reassign parameters and toBeValue to empty strings
+  // reassign displayedSelector to the current value of
+  // const [displayedSelector, setDisplayedSelector] = useState('');
+
+  if (currentSelector.length){
+
+    // update the toBe value with wahtever to
+    function handleToBeChange(e) {
+      setToBeValue(e.target.value);
+    };
+
+    function handleParameterChange(e) {
+      setParameters(e.target.value);
+    }
+
+    return (
+      <div>
+        <p>Atom ({currentAtom}): <strong>{currentAtomValue}</strong></p>
+        <p>Selector: <strong>{currentSelector}</strong></p>
+        <form>
+          <label htmlFor='expected'>To Be: </label>
+          <input type='text' id='expected' name='expected' onChange={handleToBeChange}></input>
+          
+          <label htmlFor='parameters'>Parameters: </label>
+          <input type='text' id='parameters' name='parameters' onChange={handleParameterChange}></input>
+        </form>
+
+        
+        {/* <p>To Equal: <strong>User Input</strong></p>
+        <p>Parameters: <strong>User Input</strong></p> */}
+          
+        <p>expect({currentAtomValue}).toBe({toBeValue})</p>
+      </div>
+    );
+  }
+  else {
+    return (
+      <div>      
+      </div>
+    );
+  }
 };
 
 export default DisplayTests;
+
+
+{/* <form action="/action_page.php">
+  <label for="fname">First name:</label>
+  <input type="text" id="fname" name="fname"><br><br>
+  <label for="lname">Last name:</label>
+  <input type="text" id="lname" name="lname"><br><br>
+  <input type="submit" value="Submit">
+</form> */}
