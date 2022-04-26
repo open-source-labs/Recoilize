b842fe5b69c9f76ebc041c651016f9143e3659af
Adam Allison <allisonadam81@gmail.com>
2022-04-26 08:13:38 -0700

    Parsed setter functions displaying correctly. Co-authored-by: Jordan Rice <theoriginaljordanrice@gmail.com>, William Chu <williamchu9@gmail.com>, Ryan Wallace <RyanWallace1396@gmail.com>

--- src/app/components/Testing/SelectorsButton.tsx
+++ src/app/components/Testing/SelectorsButton.tsx
@@ -2,15 +2,43 @@
 import React from 'react';
 import DisplayTests from './displayTests';
 
-const SelectorsButton: React.FC<any> = (props) => {
+const SelectorsButton: React.FC<any> = props => {
 
-  const { $selectors, selectors, atoms } = props;
+  const { selectorsFnAsStrings, selectors, atoms, setChosenSelector, onChange } = props;
 
   // completely eliminated all need for any object shenanigans or redux imports through prop drilling.
 
   const handleChange = (item) => {
     const selectorKey = item.options[item.selectedIndex].value;
-    console.log(selectorKey);
+    console.log('handleChange, selectorKey: ', selectorKey);
+    // update state with the chosen Selector
+    setChosenSelector(selectorKey);
+    const capturedFnString = selectorsFnAsStrings[selectorKey];
+    let { key, set, get } = capturedFnString;
+
+    const parser = (string) => {
+      // start a slice at _ and end at ; for each, the get and the set.
+      if (!string) return;
+      const firstPortion = string.slice(0, string.indexOf(';'));
+      const secondPortion = string.slice(string.indexOf(';') + 1, string.length);
+      let newFirstPortion = '';
+      if (firstPortion.includes('get')) newFirstPortion += ' get ';
+      if (firstPortion.includes('set')) newFirstPortion += ' set ';
+      //console.log('firstPortion ,', firstPortion)
+      //console.log('newFirstPortion ', newFirstPortion);
+      return `{ ${newFirstPortion} } => { ${secondPortion}`
+    }
+    //first portion of string is from 0 to ;
+    const displayedSelector = 
+    `Chosen selector:
+  ${key}: {
+    get: ${parser(get)}, 
+    set: ${parser(set)},
+  }`
+    //maybe need to parse function to use present as a correct string.
+    onChange(displayedSelector);
+    // onChange("displayedSelector");
+    // console.log('handleChange, selectorKey: ', selectorKey);
   }
   
   //relabeled and used a value property to capture the value on an on change above - you can now find the keys. Function needs to be completed though.
