6ef312704a8b8f7d3510c848b910ebb3c722c8a6
Anne-lise Emig <anneliseemig@gmail.com>
2023-05-01 12:23:48 -0500

    updated Tree test to be more thorough

--- src/app/components/StateTree/__tests__/Tree.unit.test.js
+++ src/app/components/StateTree/__tests__/Tree.unit.test.js
@@ -1,20 +1,47 @@
 import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
 import {getQueriesForElement, getByText} from '@testing-library/dom';
+import '@testing-library/jest-dom'
 
-import {render, fireEvent, generateStore} from '../../../tests/testing';
+import {render, fireEvent, generateStore, screen} from '../../../tests/testing';
 
 import Tree from '../Tree';
+
+// this test is loosely based on this article on rendering React tests https://betterprogramming.pub/react-testing-library-configuration-for-productive-unit-testing-5d0c446f3b3d
 
 // this is our mock state that we will use to run our tests
 import { snapshotHistoryMock } from '../../../../../mock/state-snapshot';
 
 it('Current Tree Renders', () => {
-  // define default store
+  // define default store from state snapshot
   const store = generateStore({ snapshot: snapshotHistoryMock})
-  // const {getByPlaceholderText, debug} = render(<Tree />, {providers: { store }});
-  render(<Tree />, {providers: { store }})
-});
 
-// want this test to render the tree component, then test that the tree component contains expected states, content, node deps
-// currently getting TypeError: Cannot read properties of undefined (reading 'filteredSnapshot') because we haven't provided any state
+  // here we are using our custom render function imported from testing file in tests folder. this wraps our component in a provider
+  // we then supply a store containing the snapshot
+  render(<Tree />, {providers: { store }})
+
+  // should have squares 0 through 8
+  expect(screen.queryByText('square-0')).toBeInTheDocument();
+  expect(screen.queryByText('square-1')).toBeInTheDocument();
+  expect(screen.queryByText('square-2')).toBeInTheDocument();
+  expect(screen.queryByText('square-3')).toBeInTheDocument();
+  expect(screen.queryByText('square-4')).toBeInTheDocument();
+  expect(screen.queryByText('square-5')).toBeInTheDocument();
+  expect(screen.queryByText('square-6')).toBeInTheDocument();
+  expect(screen.queryByText('square-7')).toBeInTheDocument();
+  expect(screen.queryByText('square-8')).toBeInTheDocument();
+
+  // each square should have '4 keys', so we will get an array with 9 '4 keys'
+  expect(screen.queryAllByText('4 keys')[0]).toBeInTheDocument();
+  expect(screen.queryAllByText('4 keys')[8]).toBeInTheDocument();
+
+  // each square should have node to node subscriptions
+  expect(screen.queryAllByText('nodeToNodeSubscriptions')[0]).toBeInTheDocument();
+
+  // root should have 12 keys
+  expect(screen.queryByText('12 keys')).toBeInTheDocument();
+
+  // each square should have content '-', so we will get an array of 9 '-'
+  expect(screen.queryAllByText('"-"')[0]).toBeInTheDocument();
+  expect(screen.queryAllByText('"-"')[8]).toBeInTheDocument();
+});
