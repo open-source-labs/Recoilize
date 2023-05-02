999cfba031b4ddaeba82f73e0c8beb3553f24149
Alejandro Florez <florezalejandro@gmail.com>
2023-05-02 13:27:01 -0400

    Remove tab permission

--- src/extension/contentScript.ts
+++ src/extension/contentScript.ts
@@ -1,11 +1,20 @@
-// R4 - the following line sends a message to the window and it is picked up in the window by the RecoilizeDebugger Component
+/* 
+R4 - According to Google Developer Docs: Although the execution environments 
+of content scripts and the pages that host them are isolated from each other, 
+they share access to the page's DOM. If the page wishes to communicate with 
+the content script, or with the extension via the content script, 
+it must do so through the shared DOM. An example can be accomplished using window.postMessage:
+*/
+
+// R4 - the following line sends a message to the window
+// and it is picked up in the window by the RecoilizeDebugger Component
 // once chrome tab connects with the content-script
 window.postMessage({action: 'contentScriptStarted'}, '*');
 //console.log('content script sending action to the window IN CONTENT SCRIPT');
 
 // Listen to messages from Recoilize module within dev webpage
 window.addEventListener('message', msg => {
-  console.log('contentScript message: ', msg.data);
+  console.log('contentScript.ts received the following message: ', msg.data);
   chrome.runtime.sendMessage(msg.data);
 });
 
