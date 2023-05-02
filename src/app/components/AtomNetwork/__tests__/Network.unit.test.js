db818d7376b351bd5d46c350409d1d7fc28029b8
Anne-lise Emig <anneliseemig@gmail.com>
2023-05-02 15:12:50 -0500

    updated network testing

--- src/app/components/AtomNetwork/__tests__/Network.unit.test.js
+++ src/app/components/AtomNetwork/__tests__/Network.unit.test.js
@@ -56,8 +56,8 @@ it('Atom button changes color when clicked', () => {
   // click the atom button
   fireEvent.click(atomButton);
   // class list should change to atom selected
-  expect(atomButton).toHaveClass('atomSelected')
-})
+  expect(atomButton).toHaveClass('atomSelected');
+});
 
 // when AtomP button is clicked, should display all atoms
 it('Displays all atoms when atom buttom is clicked', () => {
@@ -73,12 +73,48 @@ it('Displays all atoms when atom buttom is clicked', () => {
 
   // when network is first rendered, atoms should not be displayed
   // here, square1 returns an invisible svg element
-  const square1 = screen.getByText('square-1')
+  const square1 = screen.getByText('square-1');
   expect(square1).not.toBeVisible();
 
   // click the atom button
   fireEvent.click(atomButton);
 
-  const square = screen.getAllByText('square-1')
-  expect(square[1]).toBeVisible()
-})
+  const square = screen.getAllByText('square-1');
+  expect(square[1]).toBeVisible();
+});
+
+it('Selects specific atom when specific atom button is clicked', () => {
+  const atomNetwork = {searchValue: ''};
+  const store = generateStore({
+    atomNetwork: atomNetwork,
+    snapshot: snapshotHistoryMock,
+  });
+
+  // render the network
+  const rendered = render(<Network />, {providers: {store}});
+  // select the main atom button
+  const atomButton = screen.getByRole('button', {name: 'ATOM'});
+
+  // click the atom button
+  fireEvent.click(atomButton);
+
+  // get atom button with id "square-1"
+  const square = screen.getAllByText('square-1')[1];
+  // when square-1 atom is first rendered, it should not have atomSelected/atomNotSelected class
+  expect(square).not.toHaveClass('atomNotSelected');
+  expect(square).not.toHaveClass('atomSelected');
+
+  // click square-1 atom button
+  fireEvent.click(square);
+  // square should then be updated to be selected
+  expect(square).toHaveClass('atomSelected');
+  expect(square).not.toHaveClass('atomNotSelected');
+
+  // click square-2 button
+  const square2 = screen.getByText('square-2');
+  fireEvent.click(square2);
+  // square 2 should be selected
+  expect(square2).toHaveClass('atomSelected');
+  // square 1 should not be selected
+  expect(square).toHaveClass('atomNotSelected');
+});
