ec648f22a86e7a051564a3a5f276fed9f9a65c9c
Giovana De La Cruz <giovanacdlc@hotmail.com>
2023-05-11 10:10:40 -0700

    Clean up, animated buttons

--- src/app/components/Metrics/ComparisonGraph.tsx
+++ src/app/components/Metrics/ComparisonGraph.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useRef} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import * as d3 from 'd3';
 import {dataDurationArr} from '../../../types';
 import {useAppSelector} from '../../state-management/hooks';
