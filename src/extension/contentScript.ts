c4fdfd83512e0a2c6bd01cda79145d53c681db85
Alejandro Florez <florezalejandro@gmail.com>
2023-05-11 12:10:59 -0400

    update comments in content script

--- src/extension/contentScript.ts
+++ src/extension/contentScript.ts
@@ -1,17 +1,17 @@
 /* 
-R4 - A content script is a program that runs for any given webpage that
-the browser is currently, and *more importantly* will run WHEN the webpage LOADS
+Comment by R4 team - A content script is a program that runs for any given webpage that
+the browser is currently, and *more importantly* will run when the webpage loads
 */
 
 /* 
-R4 - According to Google Developer Docs: Although the execution environments 
+Comment by R4 team - According to Google Developer Docs: Although the execution environments 
 of content scripts and the pages that host them are isolated from each other, 
 they share access to the page's DOM. If the page wishes to communicate with 
 the content script, or with the extension via the content script, 
 it must do so through the shared DOM. An example can be accomplished using window.postMessage:
 */
 
-// R4 - the following line sends a message to the window
+// Comment by from R4 team - the following line sends a message to the window
 // and it is picked up in the window by the RecoilizeDebugger Component
 window.postMessage(
   {
@@ -28,8 +28,8 @@ window.addEventListener('message', msg => {
   // deconstruct values from message received
   const {action, payload, origininatedFrom, intendedRecipient} = msg.data;
 
-  // Check for specific message actions: moduleInitialized, persistSnapshots, recordSnapshot...
-  // To ensure backward compatibility, we chose to block by actions instead of sender/receiver (which would be ideal)
+  // Check for the specific message actions: moduleInitialized, persistSnapshots, recordSnapshot...
+  // To ensure backward compatibility with the RecoilizeDebugger, we chose to block by actions instead of sender/receiver (which would be ideal)
   if (
     action !== 'moduleInitialized' &&
     action !== 'persistSnapshots' &&
@@ -39,7 +39,7 @@ window.addEventListener('message', msg => {
     return;
   }
 
-  // create an object with the properties received in the message
+  // create an object with the properties received in the message to remove any unintended properties from other sites
   const messageReceived = {
     action,
     payload,
@@ -47,37 +47,37 @@ window.addEventListener('message', msg => {
     intendedRecipient,
   };
 
-  // create a structured clone to remove any intended messages from other sites
+  // create a structured
   const messageToSend = structuredClone(messageReceived);
 
-  // console log to test the logic of this function
+  // uncomment the following lines log to test the logic of this function by printing the message to send object
   console.log(
-    'this is messge being relayed by the content script',
+    'this is message being relayed by the content script',
     messageToSend,
   );
 
   chrome.runtime.sendMessage(messageToSend);
 });
 
-// listening for messages from the background script AKA service_worker
+// listening for messages from the service_worker --previously called the background script
 chrome.runtime.onMessage.addListener(msg => {
-  // R4 - deconstruct msg.action from the msg object into a variable with the name of action.
+  // Comment by from R4 team - deconstruct msg.action from the msg object into a variable with the name of action.
   const {action} = msg;
-  // R4 -  use a switch statement to determine what action to take based on the value of "action".
+  // Comment by from R4 team -  use a switch statement to determine what action to take based on the value of "action".
   switch (action) {
     case 'snapshotTimeTravel':
-      // R4 -  the window.postMessage() sends a message to the page's window object.
-      // R4 -  in this specific case, the Recoilize Debugger Module in the webpage (NPM package) is the intended recipient
+      // Comment by from R4 team -  the window.postMessage() sends a message to the page's window object.
+      // Comment by from R4 team -  in this  case, theRecoilize Debugger Module in the webpage is the intended recipient
       window.postMessage(msg, '*');
       break;
     case 'persistState':
-      // R4 - the window.postMessage() sends a message to the page's window object.
-      // R4 - in this specific case, the Recoilize Debugger Module in the webpage (NPM package) is the intended recipient
+      // Comment by from R4 team - the window.postMessage() sends a message to the page's window object.
+      // Comment by from R4 team - in this  case, the RecoilizeDebugger Module in the webpage is the intended recipient
       window.postMessage(msg, '*');
       break;
     case 'throttleEdit':
-      // R4 - the window.postMessage() sends a message to the page's window object.
-      // R4 - in this specific case, the Recoilize Debugger Module in the webpage (NPM package) is the intended recipient
+      // Comment by from R4 team - the window.postMessage() sends a message to the page's window object.
+      // Comment by from R4 team - in this  case, the RecoilizeDebugger Module in the webpage is the intended recipient
       window.postMessage(msg, '*');
       break;
   }
