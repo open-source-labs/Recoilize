bc67e2a564a5be0db6f02639e086b175476c8267
kaseywolff <kaseywolff5@gmail.com>
2023-05-11 15:34:07 -0500

    working, clean snapshot container + external func file

--- src/app/Containers/SnapshotContainer.tsx
+++ src/app/Containers/SnapshotContainer.tsx
@@ -16,51 +16,42 @@ import toLocalStorage from '../functions/SnapshotContainerFunctions/toLocalStora
 import {selectFilterState} from '../state-management/slices/FilterSlice';
 import {setRenderIndex} from '../state-management/slices/SnapshotSlice';
 
+
+
+// refactored from React.FC (not recommended) (5.2023 KW)
 export const SnapshotsContainer: React.FC = () => {
+    // The useDispatch hook (which is utilized in useAppDispatch -> see hooks file) returns a function. So it is assigned to a variable. Then, in the “onClick” function below, it is used to dispatch the 'setRenderIndex' action. (5.2023 KW)
   const dispatch = useAppDispatch();
 
+  // the useSelector hook (which is utilized in useAppDispatch -> see hooks file) is used to extract 'state' from the global state. We can take out any state we want to use in the component using the same way (5.2023 KW)
   const snapshotHistory = useAppSelector(
     state => state.snapshot.snapshotHistory,
   );
   const selected = useAppSelector(state => state.selected.selectedData);
 
   const renderIndex = useAppSelector(state => state.snapshot.renderIndex);
+  
   const filterData = useAppSelector(selectFilterState);
+  
   const snapshotEndRef = useRef<null | HTMLDivElement>(null);
 
   let snapshotHistoryLength = snapshotHistory.length;
 
+
+  /* <--- AUTO SCROLL TO BOTTOM -----> */
+  // this will automatically scroll us to the bottom when the snapshot history changes
   useEffect(() => {
     snapshotEndRef.current?.scrollIntoView({ behavior: 'smooth' })
   }, [snapshotHistoryLength]);
 
+
+  // creating empty array for snapshots to be pushed into as they are created
   const snapshotDivs: JSX.Element[] = [];
-  // iterate the same length of our snapshotHistory
+  // iterate the same length of our snapshotHistory to create a snapshotDiv for each
   for (let i = 0; i < snapshotHistoryLength; i++) {
     // filterFunc will return false if there is no change to state
-    const filterFunc = (): boolean => {
-      // don't use the counter for this, not reliable
-      if (i === 0) {
-        return true;
-      }
-      // checks if the filteredSnapshot object at index i has a key (atom or selector) found in the selected array. This would indicate that there was a change to that state/selector because filter is an array of objects containing differences between snapshots.
-      // console.log('filterData:', filterData)
-      if (filterData[i]) {
-        // console.log('filterData[i]', filterData[i])
-        for (let key in filterData[i].filteredSnapshot) {
-          for (let j = 0; j < selected.length; j++) {
-            if (key === selected[j].name) {
-              return true;
-            }
-          }
-        }
-      }
-      return false;
-    };
-    
-    const x: boolean = filterFunc();
+    const x: boolean = filterFunc(i, filterData, selected);
 
-    console.log(x);
     if (x === false) {
       continue;
     }
@@ -68,18 +59,8 @@ export const SnapshotsContainer: React.FC = () => {
     // renderTime is set equal to the actualDuration. If i is zero then we are obtaining actualDuration from the very first snapshot in snapshotHistory. This is to avoid having undefined filter elements since there will be no difference between snapshot at the first instance.
     let renderTime: number =
       snapshotHistory[i].componentAtomTree.treeBaseDuration;
-    // if (i === 0) {
-    //   renderTime = snapshotHistory[0].componentAtomTree.treeBaseDuration;
-    // }
-    // //Checks to see if the actualDuration within filter is an array. If it is an array then the 2nd value in the array is the new actualDuration.
-    // else if (Array.isArray(filterData[i].componentAtomTree.actualDuration)) {
-    //   renderTime = filterData[i].componentAtomTree.treeBaseDuration[1];
-    // } else {
-    //   renderTime = filterData[i].componentAtomTree.treeBaseDuration;
-    // }
+ 
 
-    // Push a div container to snapshotDivs array only if there was a change to state.
-    // The div container will contain renderTimes evaluated above.
     snapshotDivs.push(
       <div
         id={`snapshot${i}`}
@@ -103,68 +84,14 @@ export const SnapshotsContainer: React.FC = () => {
               : {}
           }
           onClick={() => {
-            timeTravelFunc(i);
+            timeTravelFunc(i, filterData);
           }}>
           Jump
         </button>
       </div>,
     );
-  }
-
-  //indexDiff is used to ensure the index of filter matches the index of the snapshots array in the backend
-  let indexDiff: number = 0;
-  if (filterData[0] && filterData[0].indexDiff) {
-    indexDiff = filterData[0].indexDiff;
-  }
-
-  // functionality to postMessage the selected snapshot index to background.js
-  const timeTravelFunc = (index: number) => {
-    // variable to store/reference connection
-    const backgroundConnection = chrome.runtime.connect();
-    //const test = chrome.extension.getBackgroundPage();
-    // post the message with index in payload to the connection
-    backgroundConnection.postMessage({
-      action: 'snapshotTimeTravel',
-      tabId: chrome.devtools.inspectedWindow.tabId,
-      payload: {
-        snapshotIndex: index + indexDiff,
-      },
-    });
   };
 
-  function prevClr() {
-    const snapshotListArr = document.querySelectorAll('.individualSnapshot');
-    for (let i = 0; i < snapshotListArr.length; i++) {
-      let index = parseInt(snapshotListArr[i].id.match(/\d+/g)[0]);
-
-      if (index < renderIndex) {
-        snapshotListArr[i].parentNode.removeChild(snapshotListArr[i]);
-      } else break;
-    }
-  }
-
-  function fwrdClr() {
-    const snapshotListArr = document.querySelectorAll('.individualSnapshot');
-
-    for (let i = snapshotListArr.length - 1; i >= 0; i--) {
-      let index = parseInt(snapshotListArr[i].id.match(/\d+/g)[0]);
-
-      if (index > renderIndex) {
-        snapshotListArr[i].parentNode.removeChild(snapshotListArr[i]);
-      } else break;
-    }
-  }
-
-  // create a function to store current data to local storage
-  const toLocalStorage = (data: any) => {
-    for (let i = 0; i < data.length; i++) {
-      // console.log('trigger toLocalStorage');
-      const jsonData = JSON.stringify(data[i]);
-      localStorage.setItem(`${i}`, jsonData);
-    }
-  };
-
-  console.log('snapshotDivs', snapshotDivs)
 
   return (
     <div className="SnapshotsContainer" data-testid="SnapshotsContainer">
@@ -200,7 +127,3 @@ export const SnapshotsContainer: React.FC = () => {
     </div>
   );
 };
-
-// export default SnapshotsContainer;
-
-// console.log(typeof SnapshotsContainer)
